cc6524acb7de7a03bb69a760ae24b8c3ec5bae2c
Carine Carvalho <carinepradecarvalho@gmail.com>
2021-04-05 09:49:46 -0300

    feat(responsivity): first responsivity adjusts

---
+++ src/script.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+
+    $('.navbar__menu-btn').on('click', function(){
+        $('.navbar__links').toggleClass('active');
+        $(this).find('i').toggleClass('fa-bars');
+        $(this).find('i').toggleClass('fa-times');
+    });
+
+    const slickOptions = {
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next slider__next-arrow">Next</button>',
+    };
+    $('.slider').slick(slickOptions);
+    $('.footer_form-button').on('click', () => {
+    const email = $('#email').val();
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : email,
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+  });
+});
